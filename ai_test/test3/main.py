94151dc27d76015cdb9d6921602c46918858c625
Michi The Cat <mrzombsoriginal@gmail.com>
2026-09-15 01:15:30 +0300

    test4

---
+++ ai_test/test3/main.py
@@ -0,0 +1,63 @@
+# 	ai_test // ☭
+# MichiTheCat-RedStar (c) 2026
+
+from pprint import pprint
+
+
+class Neuron:
+	def __init__(self, w:float=0.0, b:float=0.0, lr:float=0.01):
+		'Конструктор нейрона'
+		
+		self.W  = w  # weight        | вес входа
+		self.B  = b  # bias          | смещение
+		self.LR = lr # learning rate | скорость обучения
+	
+	
+	def Predict(self, question:float) -> float:
+		'Предсказывание'
+		
+		return self.W * question + self.B
+	
+	
+	def Train(self, question:float, correct:float):
+		'Обучение'
+		
+		error = correct - self.Predict(question)
+		self.W += self.LR * error * question
+		self.B += self.LR * error
+	
+	
+	def __str__(self):
+		'Узнать актуальный вес и смещение'
+		
+		return f'W и B: {self.W:.2f} и {self.B}'
+	
+	
+	def Educate(self, data:tuple, steps:int=1000, output:bool=False):
+		'''Итоговое обучение с поколениями
+		формат data: (правильный_ответ, чему_учат)'''
+		
+		for step in range(1, steps+1):
+			for x, y in data:
+				self.Train(x, y)
+			if (step % 1000 == 0) and output:
+				print(f'Шаг: {step}, {self}')
+
+
+# TEST
+if __name__ == '__main__':
+	n = Neuron()
+	
+	# Обучение
+	data = [(x, x*2) for x in range(10)] # пишет nan при range() большом
+	print('Дата для обучения:'); pprint(data); print('\nОбучение:')
+	n.Educate(data, 10000, True)
+	
+	print('\nНапишите число, нейрон должен умножить его на два:')
+	while True:
+		try:
+			user = float(input('Ты: '))
+		except ValueError:
+			print('Неправильный ввод!')
+		else:
+			print('ИИ:', n.Predict(user))
---
+++ ai_test/test4/ai_libs/__init__.py
@@ -0,0 +1,5 @@
+# 	ai_test/ai_libs // ☭
+# MichiTheCat-RedStar (c) 2026
+
+from . import neuron
+from . import layer
